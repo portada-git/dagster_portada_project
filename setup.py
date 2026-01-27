66fa542ecb2e29cd95bca5d62021e4d1a5ffcc53
jcanell4 <jcanell4@xtec.cat>
2026-01-27 22:31:30 +0100

    add dependence

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="dagster_portada_project",
-    version="0.0.2",
-    description='....... for PortADa project',
     author='PortADa team',
     author_email='jcbportada@gmail.com',
     license='MIT',
     packages=find_packages(exclude=["dagster_portada_project_tests"]),
     install_requires=[
-        "dagster"
+        "dagster",
+        "py_portada_data_layer @ git+https://github.com/portada-git/py_portada_data_layer",
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
