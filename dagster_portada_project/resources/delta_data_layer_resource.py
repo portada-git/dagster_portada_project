8cacae773db06e6d1ecd87642d25927d3e579850
jcanell4 <jcanell4@xtec.cat>
2026-02-11 23:46:29 +0100

    fix error

--- dagster_portada_project/resources/delta_data_layer_resource.py
+++ dagster_portada_project/resources/delta_data_layer_resource.py
@@ -24,10 +24,7 @@ class RedisConfig(ConfigurableResource):
     port: str
 
     def get_client(self, db=0):
-        if db>0:
-            r = redis.Redis(host=self.host, port=self.port, decode_responses=True, db=db)
-        else:
-            r = redis.Redis(host=self.host, port=self.port, decode_responses=True, db=db)
+        return redis.Redis(host=self.host, port=self.port, decode_responses=True, db=db)
 
     def get_sequencer(self):
         return RedisSequencer(self.get_client(db=1))
